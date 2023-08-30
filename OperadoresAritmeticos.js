725bf9cb7e803048d712054116bae15eeaab3710
Dev Wedney <devwedney@outlook.com>
2023-08-30 17:51:05 -0300

    basico.1.0

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,95 @@
+// Realize a soma ➕
+// O objetivo deste exercício é realizar a soma entre 178 e 93. Para isso:
+
+// Crie a variável primeiroNumero e atribua a ela o valor 178;
+// Crie a variável segundoNumero e atribua a ela o valor 93;
+// Crie a variável resultadoDaSoma e atribua a ela a soma entre as variáveis primeiroNumero e segundoNumero.
+
+let primeiroNumero = 178;
+let segundoNumero = 93;
+
+let resultadoDaSoma = primeiroNumero + segundoNumero;
+
+console.log(resultadoDaSoma);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Subtraindo números ➖
+// O objetivo deste exercício é realizar a subtração entre 133 e 87. Para isso:
+
+// Crie a variável primeiroNumero e atribua a ela o valor 133;
+// Crie a variável segundoNumero e atribua a ela o valor 87;
+// Crie a variável resultadoDaSubtracao e atribua a ela a subtração entre as variáveis primeiroNumero e segundoNumero.
+
+let primeiroNumero = 133;
+let segundoNumero = 87;
+
+let resultadoDaSubtracao = primeiroNumero - segundoNumero;
+
+console.log(resultadoDaSubtracao);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Divida os números ➗
+// O objetivo deste exercício é realizar a divisão de 192 por 12. Para isso:
+
+// Crie a variável dividendo e atribua a ela o valor 192;
+// Crie a variável divisor e atribua a ela o valor 12;
+// Crie a variável resultadoDaDivisao e atribua a ela a divisão da variável dividendo pela variável divisor.
+
+let dividendo = 192;
+let divisor = 12;
+
+let resultadoDaDivisao = dividendo / divisor;
+
+console.log(resultadoDaDivisao);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Multiplicando os números ✖️
+// O objetivo deste exercício é realizar a multiplicação de 133 por 13. Para isso:
+
+// Crie a variável multiplicador e atribua a ela o valor 133;
+// Crie a variável multiplicando e atribua a ela o valor 13;
+// Crie a variável resultadoDaMultiplicacao e atribua a ela a multiplicação da variável multiplicador;pela variável multiplicando.
+
+let multiplicador = 133;
+let multiplicando = 13;
+
+let resultadoDaMultiplicacao = multiplicador * multiplicando;
+
+console.log(resultadoDaMultiplicacao);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Realize a potência 🔢
+// O objetivo deste exercício é encontrar o resultado da potência de base 2 e expoente 10. Para isso:
+
+// Crie a variável base e atribua a ela o valor 2;
+// Crie a variável expoente e atribua a ela o valor 10;
+// Crie a variável potencia e atribua a ela a operação da variável base elevada a variável expoente.
+
+let base = 2;
+let expoente = 10;
+
+let potencia = base ** expoente
+
+console.log(potencia);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Resto da divisão ﹪
+// O objetivo deste exercício é encontrar o resto da divisão de 11 por 3. Para isso:
+
+// Crie a variável dividendo e atribua a ela o valor 11;
+// Crie a variável divisor e atribua a ela o valor 3;
+// Crie a variável restoDaDivisao e atribua a ela a o resto da divisão entre a variável dividendo pela variável divisor.
+
+let dividendo = 11;
+let divisor = 3;
+
+let restoDaDivisao = dividendo % divisor;
+
+console.log(restoDaDivisao);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
